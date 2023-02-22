c85ff2f89436573a51452d118e31c904795e5c99
Rahul-ai <rt878777@gmail.com>
2023-02-22 20:46:09 +0530

    Added React node

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from './Component/Header/Header';
 
 function App() {
   return (
-    <div className="App"><Header name='Rahul'></Header></div>
+    <div className="App"><Header name='Rahul'><h1>bel</h1></Header></div>
   );
 }
 
--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -1,8 +1,9 @@
 interface header{
    name:string,
    status:string,
+   children: React.ReactNode,
 }
 
 export const Header = (props:Partial<header>) =>{
-    return<h1>{props.name}{props.status}</h1>
+    return<h1>{props.name}{props.status}{props.children}</h1>
 }
