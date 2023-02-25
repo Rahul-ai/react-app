47da0badd9d5b72791cb7ca67f0b4503f5868844
Rahul-ai <rt878777@gmail.com>
2023-02-25 14:18:04 +0530

    format document

--- src/Component/FileUpload/FileUpload.tsx
+++ src/Component/FileUpload/FileUpload.tsx
@@ -1,14 +1,15 @@
 import { Api } from "../../Helpers/axios";
 
-export const FileUpload = (props:any) =>{
-    const Upload = (event:React.ChangeEvent<HTMLInputElement>) =>{
-        console.log(event.target.files);
+export const FileUpload = (props: any) => {
+
+    const Upload = (event: React.ChangeEvent<HTMLInputElement>) => {
         let formData = new FormData();
-        if(event.target.files){
-         formData.append("file",event?.target?.files[0])
-         Api.upload(formData).then((res)=>{console.log(res)});
+
+        if (event.target.files) {
+            formData.append("file", event?.target?.files[0])
+            Api.upload(formData).then((res) => { console.log(res) });
         }
-        else{
+        else {
             console.log("error");
         }
     };
