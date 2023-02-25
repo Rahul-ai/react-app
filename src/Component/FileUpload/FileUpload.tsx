da7e5a3f736d4313167a1304ca075ef8345fb0c4
Rahul-ai <rt878777@gmail.com>
2023-02-25 21:30:55 +0530

    Update file upload

--- src/Component/FileUpload/FileUpload.tsx
+++ src/Component/FileUpload/FileUpload.tsx
@@ -1,18 +1,32 @@
+import { AxiosResponse } from "axios";
+import { type } from "os";
+import { useEffect, useState } from "react";
 import { Api } from "../../Helpers/axios";
 
 export const FileUpload = (props: any) => {
 
+    const [Image, setImage] = useState<any>();
     const Upload = (event: React.ChangeEvent<HTMLInputElement>) => {
         let formData = new FormData();
 
         if (event.target.files) {
             formData.append("file", event?.target?.files[0])
-            Api.upload(formData).then((res) => { console.log(res) });
+            Api.upload(formData).then((data:any) =>{
+                let d = JSON.parse(data.data)
+                 setImage(d.path);
+                });
         }
         else {
             console.log("error");
         }
     };
 
-    return <input type="file" onChange={Upload} />
+    useEffect(()=>{
+       console.log(Image);
+    },[Image]);
+
+    return <div>
+        <input type="file" onChange={Upload} />
+        {Image && <img src={Image}></img>}
+    </div>
 }
