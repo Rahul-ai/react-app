954be5782811c4eb382a4e5242a1a3f10ff43062
Rahul-ai <rt878777@gmail.com>
2023-02-27 15:56:49 +0530

    updated form

--- src/Component/FileUpload/FileUpload.tsx
+++ src/Component/FileUpload/FileUpload.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Col, FormGroup } from "react-bootstrap";
 import { Api } from "../../Helpers/axios/axios";
 import './FileUpload.css';
 
@@ -6,21 +7,26 @@ export const FileUpload = (props: any) => {
 
     const [Image, setImage] = useState<any>();
 
-    const Upload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const Upload = (event: React.ChangeEvent<HTMLInputElement>) => {
         let formData = new FormData();
 
         if (event.target.files) {
             formData.append("file", event?.target?.files[0])
-            let d = await Api.upload(formData);
-            setImage(d.path);
+            Api.upload(formData).then((data)=>{
+                setImage(data.path);    
+                console.log(data);
+            }).catch((error)=>{
+                console.log(error)
+            })
+            
         }
         else {
             console.log("error");
         }
     };
 
-    return <div className="file_container">
+    return <Col md={4} className="file_container">
         <input type="file" onChange={Upload} />
         {Image && <img className="uploaded_image" src={Image}></img>}
-    </div>
+        </Col>
 }
--- src/Helpers/Form/form.tsx
+++ src/Helpers/Form/form.tsx
@@ -5,48 +5,51 @@ import { formInterface } from "./formInterface";
 
 interface FormClass {
     field: formInterface,
-    onChange:React.ChangeEvent<HTMLFormElement>|any,
+    onChange: React.ChangeEvent<HTMLFormElement> | any,
 };
 
 export class Form extends React.Component<FormClass>{
-    
-    renderLabel = (field:formInterface) => {
+
+    renderLabel = (field: formInterface) => {
         return <>{field.name}
-        {field.require ? <span style={{color:'rgb(240, 68, 56)'}}>*</span> : ''}
+            {field.require ? <span style={{ color: 'rgb(240, 68, 56)' }}>*</span> : ''}
         </>
     };
 
-    render(){
-    switch (this.props.field.type) {
-        case "password":
-            return <Col md={4}><FormGroup controlId={this.props.field.key}>
-            {/* {renderLabel(field)} */}
-            <FormControl type="password"
-             onChange={(e:any)=>{this.props.onChange(this.props.field.key,e.target.value)}} />
-            </FormGroup>  
-        {/* {renderError(errors[field.key])} */}
-        </Col>
+    render() {
+        switch (this.props.field.type) {
+            case "password":
+                return <Col md={4}>
+                    <FormGroup controlId={this.props.field.key}>
+                        {this.renderLabel(this.props.field)}
+                        <FormControl type="password"
+                            onChange={(e: any) => { this.props.onChange(this.props.field.key, e.target.value) }} />
+                    </FormGroup>
+                    {/* {renderError(errors[field.key])} */}
+                </Col>
 
-        case "text":
-           return <Col md={4}><FormGroup controlId={this.props.field.key}>
-            {/* {renderLabel(field)} */}
-            <FormControl type="text"  
-            onChange={(e:any)=>{this.props.onChange(this.props.field.key,e.target.value)}} />
-            </FormGroup>  
-        {/* {renderError(errors[field.key])} */}
-        </Col>
+            case "text":
+                return <Col md={4}>
+                    <FormGroup controlId={this.props.field.key}>
+                        {this.renderLabel(this.props.field)}
+                        <FormControl type="text"
+                            onChange={(e: any) => { this.props.onChange(this.props.field.key, e.target.value) }} />
+                    </FormGroup>
+                    {/* {renderError(errors[field.key])} */}
+                </Col>
 
-        case "fileUpload":
-            return <><FileUpload /></>
+            case "fileUpload":
+                return <FileUpload />
 
-        default:
-            return <Col md={4}><FormGroup controlId={this.props.field.key} 
-            onChange={(e:any)=>{this.props.onChange(this.props.field.key,e.target.value)}}>
-            {/* {renderLabel(field)} */}
-            <FormControl type="text" />
-            </FormGroup>  
-        {/* {renderError(errors[field.key])} */}
-        </Col>
+            default:
+                return <Col md={4}>
+                    <FormGroup controlId={this.props.field.key}>
+                        {this.renderLabel(this.props.field)}
+                        <FormControl type="text"
+                            onChange={(e: any) => { this.props.onChange(this.props.field.key, e.target.value) }} />
+                    </FormGroup>
+                    {/* {renderError(errors[field.key])} */}
+                </Col>
+        }
     }
-}
 };
