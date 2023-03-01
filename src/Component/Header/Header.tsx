35b3fc59cf866d9cc49309e13b6abb672d051060
Rahul-ai <rt878777@gmail.com>
2023-03-01 12:56:59 +0530

    Clean code

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { header } from "../../ComponentStructure/header"
+import { header } from "./header"
 
 export const Header = (props:Partial<header>) =>{
     return<h1>{props.name}{props.status}{props.children}</h1>
