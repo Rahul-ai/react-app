8d690bc00cd6ce7dd408161ab2b2db17afbad578
Rahul-ai <rt878777@gmail.com>
2023-04-03 19:55:09 +0530

    clean code

--- src/Component/Pagination/TablePagination.tsx
+++ src/Component/Pagination/TablePagination.tsx
@@ -1,17 +1,19 @@
 import { FormSelect, Pagination } from "react-bootstrap";
+import { Right } from "react-bootstrap/lib/Media";
 
 export const TablePagination = (props: any) => {
   return (
-    <div key={"Paginations"}> 
-    <div>
+    <div style={{display:'flex', justifyContent:"space-between"}}  key={"Paginations"} className="Paginations"> 
+    <div style={{width:80}}>
         <FormSelect onChange={(e) => props.onSelectClick(e)}>
           <option value={10}>10</option>
           <option value={20}>20</option>
           <option value={50}>50</option>
           <option value={100}>100</option>
+          {/* <option value={5}>5</option> */}
         </FormSelect>
     </div>
-    <div>
+    <div style={{float:"left"}}>
         <Pagination key={"Pagination"}>
           <Pagination.Prev />
           {[...Array(props.totalPages)].map((d, index) => {
