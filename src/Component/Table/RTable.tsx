92dc08157aa52bcec7d000dde7206aeec9f7abf1
Rahul-ai <rt878777@gmail.com>
2023-04-29 20:49:39 +0530

    clean code

--- src/Component/Table/RTable.tsx
+++ src/Component/Table/RTable.tsx
@@ -150,21 +150,21 @@ export class RTable extends React.Component<rTableInterface> {
       let data = this.props.data || this.state.items[0];
       return (
         <TabContainer key={"TabContainer"}>
-        <div className="Buttons">
-          <button type="button" className="btn btn-outline-success Export" onClick={() => { this.triggerEvent(true) }} >
-            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21"
-              viewBox="0 0 24 24" fill="#007745" stroke="white" stroke-width="2"
-              stroke-linecap="round" stroke-linejoin="round">
-              <path d="M21.2 15c.7-1.2 1-2.5.7-3.9-.6-2-2.4-3.5-4.4-3.5h-1.2c-.7-3-3.2-5.2-6.2-5.6-3-.3-5.9 1.3-7.3 4-1.2 2.5-1 6.5.5 8.8m8.7-1.6V21" /><path d="M16 16l-4-4-4 4" /></svg>
-            Export
-          </button>
+          <div className="Buttons">
+            <button type="button" className="btn btn-outline-success Export" onClick={() => { this.triggerEvent(true) }} >
+              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21"
+                viewBox="0 0 24 24" fill="#007745" stroke="white" stroke-width="2"
+                stroke-linecap="round" stroke-linejoin="round">
+                <path d="M21.2 15c.7-1.2 1-2.5.7-3.9-.6-2-2.4-3.5-4.4-3.5h-1.2c-.7-3-3.2-5.2-6.2-5.6-3-.3-5.9 1.3-7.3 4-1.2 2.5-1 6.5.5 8.8m8.7-1.6V21" /><path d="M16 16l-4-4-4 4" /></svg>
+              Export
+            </button>
 
-          <button type="button" className="btn btn-outline-primary searchButton" onClick={() => { this.triggerEvent(true) }} >
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"> </path>
-            </svg>
-            Search
-          </button>
+            <button type="button" className="btn btn-outline-primary searchButton" onClick={() => { this.triggerEvent(true) }} >
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"> </path>
+              </svg>
+              Search
+            </button>
           </div>
           <Popup trigger={trig} setSearch={this.triggerEvent} heading={<h4>Under Dev</h4>}>
             Under development
